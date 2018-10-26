2d727f09343013883b78f55025cedd6015e298dc
tmyksj <33417830+tmyksj@users.noreply.github.com>
2018-10-26 16:20:42 +0900

    カメラの映像を取得

--- src/app/components/part-play/part-play.component.ts
+++ src/app/components/part-play/part-play.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, ElementRef, OnInit, ViewChild } from "@angular/core";
 
 @Component({
   selector: "app-part-play",
@@ -7,10 +7,28 @@ import { Component, OnInit } from "@angular/core";
 })
 export class PartPlayComponent implements OnInit {
 
-  constructor() {
+  public mediaStream: MediaStream;
+  public videoHeight: number;
+  public videoWidth: number;
+
+  @ViewChild("video") private video: ElementRef;
+
+  public constructor() {
   }
 
   public ngOnInit(): void {
+    this.videoHeight = Math.floor(window.innerHeight * 0.9);
+    this.videoWidth = Math.floor(window.innerWidth * 0.9);
+
+    navigator.mediaDevices.getUserMedia({
+      audio: false,
+      video: {
+        facingMode: "environment",
+      },
+    }).then((value: MediaStream) => {
+      this.mediaStream = value;
+      this.video.nativeElement.srcObject = value;
+    });
   }
 
 }
