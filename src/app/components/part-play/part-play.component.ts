43ac86da01f7bc57a66911e0512ef15a65e577ee
tmyksj <33417830+tmyksj@users.noreply.github.com>
2018-11-18 13:28:11 +0900

    予測して音を鳴らす仕組みを実装

--- src/app/components/part-play/part-play.component.ts
+++ src/app/components/part-play/part-play.component.ts
@@ -11,9 +11,14 @@ export class PartPlayComponent implements AfterViewInit, OnDestroy, OnInit {
   public iAudioCacheDepth: number;
   public iAudioSrcList: string[];
 
+  public iCvAThreshold: number;
+  public iCvDistanceThreshold: number;
   public iCvHigh: number[];
   public iCvLow: number[];
+  public iCvRadiusMax: number;
+  public iCvRadiusMin: number;
   public iCvSrc: string;
+  public iCvYThreshold: number;
 
   public iDebugMode: boolean;
 
@@ -23,10 +28,11 @@ export class PartPlayComponent implements AfterViewInit, OnDestroy, OnInit {
 
   public oCvIsReady: boolean;
 
-  public oDebugFps: number;
   public oDebugLog: string;
 
+  public oSystemFps: number;
   public oSystemHeight: number;
+  public oSystemLoopAt: number[];
   public oSystemWidth: number;
 
   @ViewChild("video")
@@ -41,6 +47,7 @@ export class PartPlayComponent implements AfterViewInit, OnDestroy, OnInit {
   private mCvLow: any;
   private mCvM: any;
   private mCvMorphologyDefaultBorderValue: any;
+  private mCvTrackingLog: { x: number, y: number }[][];
 
   public constructor() {
     this.iAudioCacheDepth = 8;
@@ -54,22 +61,28 @@ export class PartPlayComponent implements AfterViewInit, OnDestroy, OnInit {
       "/assets/audio/b.mp3",
     ];
 
-    this.iCvHigh = [24, 24, 24, 255];
+    this.iCvAThreshold = 0.02;
+    this.iCvDistanceThreshold = 400;
+    this.iCvHigh = [42, 42, 42, 255];
     this.iCvLow = [0, 0, 0, 0];
+    this.iCvRadiusMax = 40;
+    this.iCvRadiusMin = 8;
     this.iCvSrc = "/assets/opencv.js";
+    this.iCvYThreshold = 140;
 
     this.iDebugMode = !environment.production;
 
     this.iSystemFps = 30;
-    this.iSystemHeight = 320;
-    this.iSystemWidth = 240;
+    this.iSystemHeight = 240;
+    this.iSystemWidth = 320;
 
     this.oCvIsReady = false;
 
-    this.oDebugFps = 0;
     this.oDebugLog = "";
 
+    this.oSystemFps = 0;
     this.oSystemHeight = 0;
+    this.oSystemLoopAt = [0, 0, 0, 0, 0, 0, 0, 0];
     this.oSystemWidth = 0;
   }
 
@@ -116,6 +129,8 @@ export class PartPlayComponent implements AfterViewInit, OnDestroy, OnInit {
       return;
     }
 
+    this.debugLog("audio", "play " + index);
+
     const cache: HTMLAudioElement | undefined = this.mAudioCache[index].shift();
     if (cache !== undefined) {
       cache.play();
@@ -159,20 +174,23 @@ export class PartPlayComponent implements AfterViewInit, OnDestroy, OnInit {
     script.setAttribute("src", this.iCvSrc);
     this.eVideo.nativeElement.parentElement.appendChild(script);
 
-    let cvLoopLast: number = Date.now();
+    this.oSystemLoopAt.push(Date.now());
     const cvLoop: () => void = () => {
-      const begin: number = Date.now();
-      this.cvUpdate();
-      this.wait(1000 / this.iSystemFps - (Date.now() - begin)).then(cvLoop);
+      this.oSystemLoopAt.pop();
+      this.oSystemLoopAt.unshift(Date.now());
 
-      this.oDebugFps = Math.round(1000 / (begin - cvLoopLast));
-      cvLoopLast = begin;
+      this.oSystemFps = Math.round(1000 * (this.oSystemLoopAt.length - 1)
+        / (this.oSystemLoopAt[0] - this.oSystemLoopAt[this.oSystemLoopAt.length - 1]));
+
+      this.cvUpdate();
+      this.wait(1000 / this.iSystemFps - (Date.now() - this.oSystemLoopAt[0])).then(cvLoop);
     };
 
     const cvWait: () => void = () => {
       if (window["cv"] === undefined
         || window["cv"].Mat === undefined
-        || this.eVideo.nativeElement.srcObject === null) {
+        || this.oSystemHeight === 0
+        || this.oSystemWidth === 0) {
         this.wait(100).then(cvWait);
       } else {
         this.mCv = window["cv"];
@@ -182,6 +200,7 @@ export class PartPlayComponent implements AfterViewInit, OnDestroy, OnInit {
         this.mCvLow = new this.mCv.Mat(this.oSystemHeight, this.oSystemWidth, this.mCv.CV_8UC4, this.iCvLow);
         this.mCvM = this.mCv.Mat.ones(5, 5, this.mCv.CV_8U);
         this.mCvMorphologyDefaultBorderValue = this.mCv.morphologyDefaultBorderValue();
+        this.mCvTrackingLog = [];
 
         this.oCvIsReady = true;
 
@@ -196,8 +215,6 @@ export class PartPlayComponent implements AfterViewInit, OnDestroy, OnInit {
 
   private cvUpdate(): void {
     const contours: any = new this.mCv.MatVector();
-    const dst: any = this.iDebugMode ? new this.mCv.Mat.zeros(this.oSystemHeight, this.oSystemWidth, this.mCv.CV_8UC4) : null;
-    const dstColor: any = this.iDebugMode ?  new this.mCv.Scalar(255, 255, 255, 255) : null;
     const hierarchy: any = new this.mCv.Mat();
     const src: any = new this.mCv.Mat(this.oSystemHeight, this.oSystemWidth, this.mCv.CV_8UC4);
 
@@ -208,19 +225,89 @@ export class PartPlayComponent implements AfterViewInit, OnDestroy, OnInit {
     this.mCv.dilate(src, src, this.mCvM, this.mCvAnchor, 1, this.mCv.BORDER_CONSTANT, this.mCvMorphologyDefaultBorderValue);
     this.mCv.findContours(src, contours, hierarchy, this.mCv.RETR_LIST, this.mCv.CHAIN_APPROX_SIMPLE);
 
+    const nextTrackingLog: { x: number, y: number }[][] = [];
+    const playList: number[] = [];
+
+    const t0: number = this.oSystemLoopAt[2];
+    const t1: number = this.oSystemLoopAt[1];
+    const t2: number = this.oSystemLoopAt[0];
+    const t3: number = this.oSystemLoopAt[0] + 1 / this.oSystemFps;
+
     for (let i: number = 0; i < contours.size(); i++) {
       const circle: any = this.mCv.minEnclosingCircle(contours.get(i));
 
-      if (circle.radius > 10 && circle.center.y > this.oSystemHeight / 2) {
-        this.audioPlay(Math.round(circle.center.x * this.iAudioSrcList.length / this.oSystemWidth));
-      }
+      if (this.iCvRadiusMin < circle.radius && circle.radius < this.iCvRadiusMax) {
+        let dMin: number = this.iCvDistanceThreshold * this.iCvDistanceThreshold;
+        let index: number | null = null;
 
-      if (this.iDebugMode && circle.radius > 10) {
-        this.mCv.circle(dst, circle.center, circle.radius, dstColor);
+        this.mCvTrackingLog.forEach((value: { x: number, y: number }[], i: number) => {
+          const d: number = (circle.center.x - value[0].x) * (circle.center.x - value[0].x)
+            + (circle.center.y - value[0].y) * (circle.center.y - value[0].y);
+          if (dMin > d) {
+            dMin = d;
+            index = i;
+          }
+        });
+
+        if (index !== null) {
+          const log: { x: number, y: number }[] = this.mCvTrackingLog[index].slice(0, 2);
+          log.unshift({ x: circle.center.x, y: circle.center.y });
+
+          if (log.length >= 3) {
+            const dy0: number = log[1].y - log[2].y;
+            const dy1: number = log[0].y - log[1].y;
+            const dvy0: number = dy0 / (t1 - t0);
+            const dvy1: number = dy1 / (t2 - t1);
+            const day: number = (dvy1 - dvy0) / (t2 - t1);
+            const dvy2: number = day * (t3 - t2) + dvy1;
+            const dy2: number = dvy2 * (t3 - t2);
+            const y2: number = log[0].y + dy2;
+
+            if (day > this.iCvAThreshold && y2 > this.iCvYThreshold) {
+              const dx0: number = log[1].x - log[2].x;
+              const dx1: number = log[0].x - log[1].x;
+              const dvx0: number = dx0 / (t1 - t0);
+              const dvx1: number = dx1 / (t2 - t1);
+              const dax: number = (dvx1 - dvx0) / (t2 - t1);
+              const dvx2: number = dax * (t3 - t2) + dvx1;
+              const dx2: number = dvx2 * (t3 - t2);
+              const x2: number = log[0].x + dx2;
+
+              playList.push(Math.round(x2 * this.iAudioSrcList.length / this.oSystemWidth));
+            }
+          }
+
+          nextTrackingLog.push(log);
+        } else {
+          nextTrackingLog.push([{ x: circle.center.x, y: circle.center.y }]);
+        }
       }
     }
 
+    this.mCvTrackingLog = nextTrackingLog;
+
+    this.async(() => {
+      playList.filter((value: number, i: number, array: number[]) => {
+        return array.indexOf(value) === i;
+      }).forEach((value: number) => {
+        this.audioPlay(value);
+      });
+    });
+
     if (this.iDebugMode) {
+      const dst: any = new this.mCv.Mat.zeros(this.oSystemHeight, this.oSystemWidth, this.mCv.CV_8UC4);
+      const dstColor: any = [
+        new this.mCv.Scalar(255, 255, 255, 255),
+        new this.mCv.Scalar(0, 255, 255, 255),
+        new this.mCv.Scalar(0, 0, 255, 255),
+      ];
+
+      this.mCvTrackingLog.forEach((value: { x: number, y: number }[]) => {
+        value.forEach((v: { x: number, y: number }, i: number) => {
+          this.mCv.circle(dst, { x: v.x, y: v.y }, 10, dstColor[i]);
+        });
+      });
+
       this.mCv.imshow("canvas", dst);
       dst.delete();
     }
